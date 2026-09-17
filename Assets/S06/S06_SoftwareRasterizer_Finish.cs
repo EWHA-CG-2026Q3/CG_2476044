f3c1b7881acf2929c8af334a7774946c807ec965
SeoHyeon <shyeon16@ewha.ac.kr>
2026-09-17 21:53:49 +0900

    feat: S06_SoftwareRasterizer_Finish.cs 색 수정

--- Assets/S06/S06_SoftwareRasterizer_Finish.cs
+++ Assets/S06/S06_SoftwareRasterizer_Finish.cs
@@ -8,8 +8,8 @@ public class S06_SoftwareRasterizer_Finish : MonoBehaviour
     [SerializeField] private Vector2 vertexA = new Vector2(220, 200);
     [SerializeField] private Vector2 vertexB = new Vector2(30, 30);
     [SerializeField] private Vector2 vertexC = new Vector2(230, 50);
-    [SerializeField] private Color fillColor = new Color(1f, 0.6f, 0.2f, 1f);
-    [SerializeField] private Color backgroundColor = new Color(0f, 0f, 0f, 1f);
+    [SerializeField] private Color fillColor = new Color(1f, 0.4f, 0.6f, 1f);
+    [SerializeField] private Color backgroundColor = new Color(1f, 1f, 1f, 1f);
 
     private Texture2D canvasTexture;
     private RawImage targetImage;
